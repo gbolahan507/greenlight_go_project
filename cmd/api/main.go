6a4e775dc6f653149ad07fd97d6ba8302691fc6f
Habeeb <ahabeeblahi1@gmail.com>
2023-02-19 01:20:59 +0100

    sql migration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -8,17 +10,25 @@ import (
 	"os"
 	"time"
 
+	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number.
 const version = "2.0.0"
 
 // Define a config struct to hold all the configuration settings for our application.
+type db struct {
+	dsn          string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
 type config struct {
 	port int
 	env  string
+	db   db
 }
-
 
 type application struct {
 	config config
@@ -30,10 +40,30 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
 	flag.StringVar(&cfg.env, "env", "development", "Enviroment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+
+	// flag.IntVar(&cfg.db.maxOpenConns, "db-dsn", 25, "PostgreSQL max open connections")
+	// flag.IntVar(&cfg.db.maxIdleConns, "db-dsn", 25, "PostgreSQL max Idle connections")
+	// flag.StringVar(&cfg.db.maxIdleTime, "db-dsn", "15m", "PostgreSQL max Idle connections time")
 
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer a call to db.Close() so that the connection pool is closed before the
+	// main() function exits.
+
+	defer db.Close()
+
+	// Also log a message to say that the connection pool has been successfully
+	// established.
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -51,8 +81,29 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, serve.Addr)
-	err := serve.ListenAndServe()
+	err = serve.ListenAndServe()
 	log.Fatal(err)
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 
 }
 
@@ -60,7 +111,9 @@ func main() {
 
 // go run ./cmd/api -port=3030 -env=production
 
-
 // Read on
 
 // json decoding nuances   pg 80
+
+// password: pa55word
+// user : greenlight
--- internal/model/movies.go
+++ internal/model/movies.go
@@ -7,12 +7,12 @@ import (
 
 type Movie struct {
 	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
 	Runtime   Runtime   `json:"runtime,omitempty,string"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
-	CreatedAt time.Time `json:"-"`
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
